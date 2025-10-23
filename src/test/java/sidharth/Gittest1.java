1f37e8e0ecf30b2a8ea2cf925589d8e6ebd10c51
SIDHARTHG7 <gsidharth4007@gmail.com>
2025-10-23 21:17:04 +0530

    sidharth

---
+++ src/test/java/sidharth/Gittest1.java
@@ -0,0 +1,15 @@
+package sidharth;
+
+public class Gittest1 {
+
+	public static void main(String args[]) {
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 5 - i; j >= 1; j--)
+				System.out.print("  ");
+			for (int k = 1; k <= i; k++)
+				System.out.print("* ");
+			System.out.println();
+		}
+	}
+
+}
