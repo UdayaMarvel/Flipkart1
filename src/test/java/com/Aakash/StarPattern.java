3ac87abb1451ff38627f9b6fdd09d5dfb8d2a049
Aakash00 <aakashravi2998@gmail.com>
2025-10-24 18:05:13 +0530

    change the sysout in aakash branch

--- src/test/java/com/Aakash/StarPattern.java
+++ src/test/java/com/Aakash/StarPattern.java
@@ -14,7 +14,7 @@ public class StarPattern {
 	            for (int k = 1; k <= (2 * i - 1); k++) {
 	                System.out.print("*");
 	            }
-	            System.out.println();
+	            System.out.println("1233255356");
 	        }
 	    }
 	
