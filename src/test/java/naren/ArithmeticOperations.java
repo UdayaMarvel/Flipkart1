065c47ef065278fa565737fdf60da68028bb5105
mnarenkumar1299-ux <m.narenkumar1299@gmail.com>
2025-10-29 11:44:20 +0530

    Update ArithmeticOperations.java

--- src/test/java/naren/ArithmeticOperations.java
+++ src/test/java/naren/ArithmeticOperations.java
@@ -6,7 +6,7 @@ public class ArithmeticOperations {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter 2nd number: ");
+        System.out.print("Enter 1st number: ");
         int a = sc.nextInt();
 
         System.out.print("Enter second number: ");
